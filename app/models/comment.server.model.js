0709a8ce24694158357172bdcaa849a47f4a83fe
Martin <gosevski@gmail.com>
2015-05-16 15:48:43 +0200

    nz

---
+++ app/models/comment.server.model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Comment Schema
+ */
+var CommentSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Comment name',
+		trim: true
+	},
+    comment: {
+		type: String,
+		default: '',
+		required: 'Please fill Comment comment',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Comment', CommentSchema);
