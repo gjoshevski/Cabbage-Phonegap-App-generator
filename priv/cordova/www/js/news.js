69887e183332a066bb03ade77187aa04a6a7f48b
Martin <gosevski@gmail.com>
2015-06-02 21:34:43 +0200

    - bcard get/update

--- priv/cordova/www/js/news.js
+++ priv/cordova/www/js/news.js
@@ -1,28 +1,45 @@
 var CabbageNews = {
 	pullInterval: 5*1000,
 	timerId: undefined,
+
 	init: function() {
 		console.log('cabbage-news | init');
-
+		this.start();
+	},
+	start: function() {
 		var self = this;
-		this.timerId = window.setInterval(function() { self.pullNews() }, this.pullInterval);
+		this.timerId = window.setTimeout(function() { self.pullNews() }, this.pullInterval);
 	},
 	stop: function() {
 		if(this.timerId !== undefined) {
-			window.clearInterval(this.timerId);
+			window.setTimeout(this.timerId);
 			this.timerId = undefined;
 		}
 	},
 	pullNews: function()  {
-		this.showNotification("Test notif");
+		this.stop();
+		this.getNews();
 	},
 	showNotification: function(text) {
-		console.log(text);
 		var now = new Date().getTime();
 		cordova.plugins.notification.local.schedule({
 			id: 101,
 			text: text,
 			at: now
 		});	
+	},
+	getNews: function() {
+		var self = this;
+		$.get({
+			type: 'GET',
+			url: CabbageConf.endpoint + '/news'
+		})
+		.done(function(response) {
+			self.showNotification('Test notif');
+			self.start();
+		})
+		.fail(function() {
+			console.log('cabbage-news | news load error')
+		});
 	}
 }
